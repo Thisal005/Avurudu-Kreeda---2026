a0c409e7ad912efd0a3fd71e4f0a3e96d7927b24
Thisal005 <thisalth2@gmail.com>
2026-04-11 08:13:17 +0530

    feat: add WelcomePopup component to RootLayout for enhanced user experience

--- avurudu-kreeda-2026/src/app/layout.tsx
+++ avurudu-kreeda-2026/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
 import "./globals.css";
 import BackgroundMusic from "@/components/BackgroundMusic";
+import WelcomePopup from "@/components/WelcomePopup";
 
 const geistSans = Outfit({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         <BackgroundMusic />
+        <WelcomePopup />
         {children}
       </body>
     </html>
